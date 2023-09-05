21308c79f72d835780da68e0fea172f6b4fe76d3
jobarv <jbarrera@motiontech.com.mx>
2023-09-05 11:52:28 -0600

    Formulario de contacto

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
+const buttonSubmit = document.querySelector('#submit');
+
 function menuOnClick() {
     document.getElementById("menu-bar").classList.toggle("change");
     document.getElementById("nav").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
   }
+
+  formulario.addEventListener('submit', (event) => {
+    event.preventDefault()
+    buttonSubmit.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>'
+    Swal.fire({
+        title: '¡Gracias por contáctarnos, en breve nos comunicaremos contigo!',
+        icon: 'success',
+        confirmButtonText: 'OK'
+      })
+    })
