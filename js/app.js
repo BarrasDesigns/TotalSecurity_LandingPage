9d2f20099bc0cc3b864acbba3d5042a641817a2e
jobarv <jbarrera@motiontech.com.mx>
2023-09-06 15:09:18 -0600

    posts en archivo

--- js/app.js
+++ js/app.js
@@ -1,10 +1,32 @@
-let xhr = new XMLHttpRequest();
+// let xhr = new XMLHttpRequest();
 
-xhr.open('GET', './feed.php', true);
+// xhr.open('GET', './feed.php', true);
+// xhr.onreadystatechange = function () {
+//   if (xhr.readyState === 4 && xhr.status === 200) {
+//     console.log(xhr.responseText)
+//     // document.getElementById('latest-posts').innerHTML = xhr.responseText;
+//   }
+// };
+// xhr.send();
+var xhr = new XMLHttpRequest();
+xhr.open('GET', '../posts.xml', true);
 xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    document.getElementById('latest-posts').innerHTML = xhr.responseText;
-  }
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        var xmlDoc = xhr.responseXML;
+        var items = xmlDoc.getElementsByTagName('post');
+        var html = '<ul>';
+
+        for (var i = 0; i < items.length; i++) {
+            var title = items[i].getElementsByTagName('Title')[0].textContent;
+            var link = items[i].getElementsByTagName('Permalink')[0].textContent;
+            var description = items[i].getElementsByTagName('Content')[0].textContent;
+            
+            html += '<li><a href="' + link + '">' + title + '</a><p>' + description + '</p></li>';
+        }
+
+        html += '</ul>';
+        document.getElementById('latest-posts').innerHTML = html;
+    }
 };
 xhr.send();
 
