4174b2734d63331fb6fdb0b9dcd18ab31b43cd0c
jobarv <jbarrera@motiontech.com.mx>
2023-09-06 18:30:33 -0600

    estilos finales en footer

--- js/app.js
+++ js/app.js
@@ -1,34 +1,26 @@
-// let xhr = new XMLHttpRequest();
-
-// xhr.open('GET', './feed.php', true);
-// xhr.onreadystatechange = function () {
-//   if (xhr.readyState === 4 && xhr.status === 200) {
-//     console.log(xhr.responseText)
-//     // document.getElementById('latest-posts').innerHTML = xhr.responseText;
-//   }
-// };
-// xhr.send();
-var xhr = new XMLHttpRequest();
+let xhr = new XMLHttpRequest();
 xhr.open('GET', '../posts.xml', true);
 xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
-        var xmlDoc = xhr.responseXML;
-        var items = xmlDoc.getElementsByTagName('post');
-        var html = '<ul>';
+        let xmlDoc = xhr.responseXML;
+        let items = xmlDoc.getElementsByTagName('post');
+        let html = '<ul>';
 
-        for (var i = 0; i < items.length; i++) {
-            var title = items[i].getElementsByTagName('Title')[0].textContent;
-            var link = items[i].getElementsByTagName('Permalink')[0].textContent;
-            var description = items[i].getElementsByTagName('Content')[0].textContent;
+        for (let i = 0; i < items.length; i++) {
+            let title = items[i].getElementsByTagName('Title')[0].textContent;
+            let link = items[i].getElementsByTagName('Permalink')[0].textContent;
+            let description = items[i].getElementsByTagName('Content')[0].textContent;
             
-            html += '<li><a href="' + link + '">' + title + '</a><p>' + description + '</p></li>';
+            html += '<li><a href="' + link + '">' + title + '<p>' + description + '</p></a></li>';
         }
 
         html += '</ul>';
-        document.getElementById('latest-posts').innerHTML = html;
-    }
+        document.getElementById('blog__item').innerHTML = html;
+      }
+      document.getElementById('footer__titulos').innerHTML = html;
 };
 xhr.send();
+
 
 function menuOnClick() {
   document.getElementById("menu-bar").classList.toggle("change");
