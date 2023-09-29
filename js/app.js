f63c7b35d0d0cf6336b77953ca1384d352876e2f
jobarv <jbarrera@motiontech.com.mx>
2023-09-29 08:09:51 -0600

    cambios en carrusel

--- js/app.js
+++ js/app.js
@@ -1,3 +1,58 @@
+// let slideIndex = 0;
+
+
+// showSlides();
+
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("slide-image");
+//   let dots = document.getElementsByClassName("carousel-dot");
+
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}    
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";  
+//   dots[slideIndex-1].className += " active";
+//   setTimeout(showSlides, 2000); 
+// }
+
+let slideIndex = 1;
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  //alert('showslides('+ n +')\nslideIndex='+slideIndex);
+  let i;
+  let slides = document.getElementsByClassName("slide-image");
+  let dots = document.getElementsByClassName("carousel-dot");
+  if (n==undefined){n = ++slideIndex}
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  
+  setTimeout(showSlides, 1000); // Change image every 5 seconds
+}
+
+
+
 let xhr = new XMLHttpRequest();
 xhr.open('GET', './posts.xml', true);
 xhr.onreadystatechange = function () {
