2c54453faa662c25afa87a7702175763acfc92b0
jobarv <jbarrera@motiontech.com.mx>
2024-02-25 11:12:47 -0600

    Correccion en carrusel

--- js/app.js
+++ js/app.js
@@ -1,93 +1,104 @@
-// let slideIndex = 0;
+let slideIndex = 0;
+let slides = document.getElementsByClassName("slide-image");
+let dots = document.getElementsByClassName("carousel-dot");
 
 
-// showSlides();
+showSlides();
+function showSlides() {
 
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("slide-image");
-//   let dots = document.getElementsByClassName("carousel-dot");
+  let i;
 
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    dots[i].classList.remove("active");
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.add("active");
+
+  setTimeout(showSlides, 2000); // Cambiar cada 2 segundos
+  console.log(slideIndex);
+}
+
+function changeSlide(n, slide) {
+  if (changeSlide.addEventListener("click")) {
+    switchSlide();
+  }
+  else {
+    showSlides(slideIndex += n);
+  }
+}
+
+// function currentSlide(n) {
+//   slideIndex = n;
+//   if (slideIndex >= slides.length) { slideIndex = 0; }
+//   if (slideIndex < 0) { slideIndex = slides.length - 1; }
+//   showSlides();
+
+// }
+// function showSlides(n) {
+//   if (n === undefined) { n = ++slideIndex }
+//   if (n >= slides.length) { slideIndex = 0 }
+//   if (n < 0) { slideIndex = slides.length - 1 }
+
+//   for (let i = 0; i < 5 ; i++) {
+//       slides[i].style.display = "none";
+//       dots[i].classList.remove("active");
+//     }
+
+//     slides[slideIndex].style.display = "block";
+//     dots[slideIndex].classList.add("active");
+
+//     setTimeout(() => {
+//       showSlides(slideIndex + 1);
+//     }, 100);
 //   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}    
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
+// console.log(currentSlide.length);
+
+// for (let i = 0; i < 5; i++) {
+//     dots[i].addEventListener("click", function () {
+//         currentSlide(i);
+//       });
+//     }
+
+
+// let xhr = new XMLHttpRequest();
+// xhr.open('GET', './posts.xml', true);
+// xhr.onreadystatechange = function () {
+//   if (xhr.readyState === 4 && xhr.status === 200) {
+//     let xmlDoc = xhr.responseXML;
+//     let items = xmlDoc.getElementsByTagName('post');
+//     let html = '<ul>';
+
+//     for (let i = 0; i < items.length; i++) {
+//       let title = items[i].getElementsByTagName('Title')[0].textContent;
+//       let link = items[i].getElementsByTagName('Permalink')[0].textContent;
+//       let image = items[i].getElementsByTagName('ImageFeatured')[0].textContent;
+
+//       //let description = items[i].getElementsByTagName('Content')[0].textContent;
+
+//       html += '<li><a href="' + link + '"><h4>' + title + '</h4> <img src= "' + image + '" width="300" height="200"/></a></li>';
+//     }
+
+//     html += '</ul>';
+//     document.getElementById('blog__body').innerHTML = html;
 //   }
-//   slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " active";
-//   setTimeout(showSlides, 2000); 
+// };
+// xhr.send();
+
+
+// function menuOnClick() {
+//   document.getElementById("menu-bar").classList.toggle("change");
+//   document.getElementById("nav").classList.toggle("change");
+//   document.getElementById("menu-bg").classList.toggle("change-bg");
 // }
 
-let slideIndex = 1;
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  //alert('showslides('+ n +')\nslideIndex='+slideIndex);
-  let i;
-  let slides = document.getElementsByClassName("slide-image");
-  let dots = document.getElementsByClassName("carousel-dot");
-  if (n==undefined){n = ++slideIndex}
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  
-  setTimeout(showSlides, 1000); // Change image every 5 seconds
-}
-
-
-
-let xhr = new XMLHttpRequest();
-xhr.open('GET', './posts.xml', true);
-xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        let xmlDoc = xhr.responseXML;
-        let items = xmlDoc.getElementsByTagName('post');
-        let html = '<ul>';
-
-        for (let i = 0; i < items.length; i++) {
-            let title = items[i].getElementsByTagName('Title')[0].textContent;
-            let link = items[i].getElementsByTagName('Permalink')[0].textContent;
-            let image = items[i].getElementsByTagName('ImageFeatured')[0].textContent;
-            
-            //let description = items[i].getElementsByTagName('Content')[0].textContent;
-            
-            html += '<li><a href="' + link + '"><h4>' + title + '</h4> <img src= "'+image+'" width="300" height="200"/></a></li>';
-        }
-
-        html += '</ul>';
-        document.getElementById('blog__body').innerHTML = html;
-    }
-};
-xhr.send();
-
-
-function menuOnClick() {
-  document.getElementById("menu-bar").classList.toggle("change");
-  document.getElementById("nav").classList.toggle("change");
-  document.getElementById("menu-bg").classList.toggle("change-bg");
-}
-
-document.getElementById('envia').onclick = function(){
-  Swal.fire({
-    icon: "success",
-    title: "¡Gracias por Contactárnos!",
-    text: "En breve nos comunicaremos contigo."
-  });
-}
+// document.getElementById('envia').onclick = function () {
+//   Swal.fire({
+//     icon: "success",
+//     title: "¡Gracias por Contactárnos!",
+//     text: "En breve nos comunicaremos contigo."
+//   });
+// }
